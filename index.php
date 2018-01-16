8c055138268e7ca6872223ce3800f5ce0a0dd122
akshay <akshay@tvisha.in>
2018-01-16 16:19:14 +0530

    my first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Hello world!</title>
+  <meta charset="UTF-8">
+  <link rel="stylesheet" href = "css/style.css" />
+  <script src="js/vendor/jquery.min.js"></script>
+  <script src="js/index.js"></script>
+</head>
+  <body>
+    <div class = "main-container">
+      <div class="main-wrapper">
+        <div>
+          <span class="hello-text">Hello World!</span>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
